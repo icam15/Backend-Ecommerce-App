5d34e780c88edfc9959410cc455a6bae37937f03
icam15 <159884438+icam15@users.noreply.github.com>
2024-09-22 10:59:40 +0700

    setup router controller service user (#7)
    Co-authored-by: syam achmad ali <biasnkiza.gmail.com>

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,16 +1,21 @@
 import express, { Router } from "express";
 import { AuthRouter } from "./auth-router";
+import { UserRouter } from "./user-router";
+import { authenticationUser } from "../middleware/auth/authentication";
 
 export class RootRouter {
   private router: Router;
   private authRouter: AuthRouter;
+  private userRouter: UserRouter;
   constructor() {
     this.router = Router();
     this.authRouter = new AuthRouter();
+    this.userRouter = new UserRouter();
     this.initializeRouter();
   }
   private initializeRouter(): void {
     this.router.use("/auth", this.authRouter.getRouter());
+    this.router.use("/user", authenticationUser, this.userRouter.getRouter());
   }
   getRouter() {
     return this.router;
