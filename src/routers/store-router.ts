926ee9a17d53c72b9aced866ded8631214f1d905
syam achmad ali <biasnkiza.gmail.com>
2024-09-28 15:40:54 +0700

    add store controller and service

--- src/routers/store-router.ts
+++ src/routers/store-router.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { StoreController } from "../controllers/store-controller";
 
 export class StoreRouter {
   private router: Router;
+  private storeController: StoreController;
   constructor() {
     this.router = Router();
+    this.storeController = new StoreController();
     this.initializeRoute();
   }
 
