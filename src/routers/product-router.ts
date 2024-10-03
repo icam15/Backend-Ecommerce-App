79417eab6c20d0b84116ad5e69da2cb75c373c20
syam achmad ali <biasnkiza.gmail.com>
2024-10-03 15:50:47 +0700

    add product controller and service

--- src/routers/product-router.ts
+++ src/routers/product-router.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { ProductController } from "../controllers/product-controller";
 
 export class ProductRouter {
   private router: Router;
+  private productController: ProductController;
   constructor() {
     this.router = Router();
+    this.productController = new ProductController();
   }
 
   private initializeRoute(): void {}
