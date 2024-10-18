e35d89dfeab824ce30c24a964877bf405f02c12b
syam achmad ali <bianskiza@gmail.com>
2024-10-18 20:13:58 +0700

    add order controller, service and router

---
+++ src/routers/order-router.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { OrderController } from "../controllers/order-controller";
+
+export class OrderRouter {
+  private router: Router;
+  private orderController: OrderController;
+  constructor() {
+    this.router = Router();
+    this.orderController = new OrderController();
+    this.initializeRouter();
+  }
+
+  private initializeRouter() {}
+  getRouter(): Router {
+    return this.router;
+  }
+}
